e8a7c69024fdcd2d2ce42c7a5267d82781fc8073
Kellan Mythen <kellanmythen@gmail.com>
2026-06-29 10:25:16 -0700

    kscope: draw spectrum as rectangle bars (manual polygon fill rendered nothing)

--- kscope/js/kscope_ui.js
+++ kscope/js/kscope_ui.js
@@ -47,38 +47,30 @@ function paint() {
     var w = box.rect[2] - box.rect[0];
     var h = box.rect[3] - box.rect[1];
     try {
-        var pts = [];
-        for (var px = 0; px <= w; px += 2) {
-            var freq = FMIN * Math.pow(FMAX / FMIN, px / w);
-            var mag = readbin(Math.round(freq / BINHZ));
-            var db = 20 * Math.log(Math.max(mag, 1e-7)) / Math.LN10;
-            var v = (db + 80) / 60;   // -80..-20 dB -> 0..1 (fuller display)
-            if (v < 0) v = 0; if (v > 1) v = 1;
-            pts.push([px, h - v * h]);
-        }
         with (mgraphics) {
+            // background
             set_source_rgba(0.055, 0.055, 0.075, 1.0);
             rectangle(0, 0, w, h); fill();
 
+            // frequency grid
             set_source_rgba(1, 1, 1, 0.06);
-            set_line_width(1);
             var grids = [100, 1000, 10000];
             for (var g = 0; g < grids.length; g++) {
                 var gx = w * Math.log(grids[g] / FMIN) / Math.log(FMAX / FMIN);
-                move_to(gx, 0); line_to(gx, h); stroke();
+                rectangle(gx, 0, 1, h); fill();
             }
 
-            if (pts.length > 1) {
-                move_to(0, h);
-                for (var i = 0; i < pts.length; i++) line_to(pts[i][0], pts[i][1]);
-                line_to(w, h); close_path();
-                set_source_rgba(0.91, 0.20, 0.11, 0.88);
-                fill();
-                set_source_rgba(1.0, 0.45, 0.30, 0.95);
-                set_line_width(2);
-                move_to(pts[0][0], pts[0][1]);
-                for (var j = 1; j < pts.length; j++) line_to(pts[j][0], pts[j][1]);
-                stroke();
+            // spectrum as vertical bars (rectangle fill is proven to render)
+            set_source_rgba(0.91, 0.22, 0.12, 0.95);
+            var step = 3;
+            for (var px = 0; px < w; px += step) {
+                var freq = FMIN * Math.pow(FMAX / FMIN, px / w);
+                var mag = readbin(Math.round(freq / BINHZ));
+                var db = 20 * Math.log(Math.max(mag, 1e-7)) / Math.LN10;
+                var v = (db + 80) / 60;   // -80..-20 dB -> 0..1
+                if (v < 0) v = 0; if (v > 1) v = 1;
+                var bh = v * h;
+                if (bh > 0.5) { rectangle(px, h - bh, step - 0.6, bh); fill(); }
             }
         }
     } catch (e) {
