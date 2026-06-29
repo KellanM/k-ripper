5edd21469d8f669a3218beaabde3a5e7be2fdccf
Kellan Mythen <kellanmythen@gmail.com>
2026-06-28 21:32:33 -0700

    kscope: paint heartbeat (green box) + paint counter + error catch in jsui

--- kscope/js/kscope_ui.js
+++ kscope/js/kscope_ui.js
@@ -40,42 +40,53 @@ function readbin(b) {
     return bins[b];
 }
 
+var paintCount = 0;
+
 function paint() {
+    paintCount++;
     var w = box.rect[2] - box.rect[0];
     var h = box.rect[3] - box.rect[1];
-    var pts = [];
-    for (var px = 0; px <= w; px += 2) {
-        var freq = FMIN * Math.pow(FMAX / FMIN, px / w);
-        var mag = readbin(Math.round(freq / BINHZ));
-        var db = 20 * Math.log(Math.max(mag, 1e-7)) / Math.LN10;
-        var v = (db + 90) / 90;
-        if (v < 0) v = 0; if (v > 1) v = 1;
-        pts.push([px, h - v * h]);
-    }
-    with (mgraphics) {
-        set_source_rgba(0.055, 0.055, 0.075, 1.0);
-        rectangle(0, 0, w, h); fill();
-
-        set_source_rgba(1, 1, 1, 0.06);
-        set_line_width(1);
-        var grids = [100, 1000, 10000];
-        for (var g = 0; g < grids.length; g++) {
-            var gx = w * Math.log(grids[g] / FMIN) / Math.log(FMAX / FMIN);
-            move_to(gx, 0); line_to(gx, h); stroke();
+    try {
+        var pts = [];
+        for (var px = 0; px <= w; px += 2) {
+            var freq = FMIN * Math.pow(FMAX / FMIN, px / w);
+            var mag = readbin(Math.round(freq / BINHZ));
+            var db = 20 * Math.log(Math.max(mag, 1e-7)) / Math.LN10;
+            var v = (db + 90) / 90;
+            if (v < 0) v = 0; if (v > 1) v = 1;
+            pts.push([px, h - v * h]);
         }
+        with (mgraphics) {
+            set_source_rgba(0.055, 0.055, 0.075, 1.0);
+            rectangle(0, 0, w, h); fill();
 
-        if (pts.length > 1) {
-            move_to(0, h);
-            for (var i = 0; i < pts.length; i++) line_to(pts[i][0], pts[i][1]);
-            line_to(w, h); close_path();
-            set_source_rgba(0.91, 0.20, 0.11, 0.88);
-            fill();
-            set_source_rgba(1.0, 0.45, 0.30, 0.95);
-            set_line_width(1.5);
-            move_to(pts[0][0], pts[0][1]);
-            for (var j = 1; j < pts.length; j++) line_to(pts[j][0], pts[j][1]);
-            stroke();
+            set_source_rgba(1, 1, 1, 0.06);
+            set_line_width(1);
+            var grids = [100, 1000, 10000];
+            for (var g = 0; g < grids.length; g++) {
+                var gx = w * Math.log(grids[g] / FMIN) / Math.log(FMAX / FMIN);
+                move_to(gx, 0); line_to(gx, h); stroke();
+            }
+
+            if (pts.length > 1) {
+                move_to(0, h);
+                for (var i = 0; i < pts.length; i++) line_to(pts[i][0], pts[i][1]);
+                line_to(w, h); close_path();
+                set_source_rgba(0.91, 0.20, 0.11, 0.88);
+                fill();
+                set_source_rgba(1.0, 0.45, 0.30, 0.95);
+                set_line_width(2);
+                move_to(pts[0][0], pts[0][1]);
+                for (var j = 1; j < pts.length; j++) line_to(pts[j][0], pts[j][1]);
+                stroke();
+            }
+
+            // DIAGNOSTIC heartbeat: bright green box top-left proves paint() runs
+            set_source_rgba(0.20, 0.90, 0.35, 1.0);
+            rectangle(4, 4, 18, 18); fill();
         }
+    } catch (e) {
+        post("[kscope] paint error: " + e.message + "\n");
     }
 }
 
@@ -83,5 +94,6 @@ function bang() { mgraphics.redraw(); }
 
 function dbg() {
     post("[kscope] ui max=" + lastMax.toFixed(5) +
-         " size=" + (box.rect[2] - box.rect[0]) + "x" + (box.rect[3] - box.rect[1]) + "\n");
+         " size=" + (box.rect[2] - box.rect[0]) + "x" + (box.rect[3] - box.rect[1]) +
+         " paints=" + paintCount + "\n");
 }
